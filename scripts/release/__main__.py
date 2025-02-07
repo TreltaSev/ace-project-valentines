dec12790a322c0f094bc2e3941162aa9292d3a92
Trelta Sebastion <treltasev@gmail.com>
2025-02-07 14:14:31 -0500

    [release] awd

--- scripts/release/__main__.py
+++ scripts/release/__main__.py
@@ -1,11 +1,15 @@
 import click
 from pyucc import console, colors, symbols
 import sys
+import os
 
-@click.group()
+@click.group(invoke_without_command=True)
 @click.pass_context
-def cli(ctx):
-    print("...")
+@click.option("--releasemessage", prompt="Title this release")
+def cli(ctx, releasemessage: str):
+    os.system("git add .")
+    os.system(f'git commit -m "[release] {releasemessage}"')
+    # os.system("git push")
 
 if __name__ == "__main__":
     cli()
