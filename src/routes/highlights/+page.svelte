e0fca0d0e62b69d8a781f01779bf227c3d09dfe6
TreltaSev <treltasev@gmail.com>
2025-02-10 19:22:07 -0500

    add/ base routes

---
+++ src/routes/highlights/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Flex, Button, Span, Page } from '@ui';
+</script>
+
+<Page._>
+	<span>Highlights Page</span>
+</Page._>
---
+++ src/routes/history/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import { Flex, Button, Span, Page } from '@ui';
+</script>
+
+<Page._>
+	<span>History Page</span>
+</Page._>
